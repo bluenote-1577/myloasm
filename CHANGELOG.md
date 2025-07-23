e23a28906856118a60157ef7da8a87a517c108c8
bluenote-1577 <jimshawster@gmail.com>
2025-07-23 09:44:16 -0400

    added fasta test

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+v0.1.1 - DATE TODO
+
+- allow fasta reads (https://github.com/bluenote-1577/myloasm/issues/3)
--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -76,3 +76,31 @@ fn test_nanopore_basic_run_48kb_plasmid() -> Result<(), Box<dyn std::error::Erro
     
     Ok(())
 }
+
+#[test]
+fn test_nanopore_basic_run_48kb_plasmid_FASTA() -> Result<(), Box<dyn std::error::Error>> {
+    let temp_dir = TempDir::new()?;
+    let output_dir = temp_dir.path().join("output");
+    
+    let test_input = Path::new("tests/reads/40kb_plas.fa");
+
+    let mut cmd = Command::cargo_bin("myloasm")?;
+    cmd.arg(test_input.to_str().unwrap())
+        .arg("-o")
+        .arg(output_dir.to_str().unwrap())
+        .arg("-t")
+        .arg("20"); // Use fewer threads for testing
+    
+    cmd.assert().success();
+    
+    // Check output directory exists and contains expected files
+    assert!(output_dir.exists());
+    assert!(output_dir.join("assembly_primary.fa").exists());
+
+    //Check the assembly file has > 1.8kb and <2kb and has only one contig
+    let assembly_file = output_dir.join("assembly_primary.fa");
+    let assembly_content = fs::read_to_string(&assembly_file)?;
+    let contigs: Vec<&str> = assembly_content.lines().filter(|line| line.starts_with('>')).collect();
+    println!("Contigs found: {:?}", contigs);
+    Ok(())
+}
